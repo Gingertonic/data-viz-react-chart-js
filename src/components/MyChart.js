84fc1d0725273c0cdd58d2bc0d7a6a8a5a56410d
Gingertonic <bethmschofield@gmail.com>
2020-04-18 14:58:25 +0100

    fix color change bug with redraw prop

--- src/components/MyChart.js
+++ src/components/MyChart.js
@@ -2,20 +2,25 @@ import React, { Component } from 'react'
 import { Bar } from "react-chartjs-2";
 
 export default class MyChart extends Component {
+    theChart = React.createRef()
+
     state = {
         chosenData: []
     }
 
     static getDerivedStateFromProps(props, state){
         if(props.data !== state.chosenData){
-                const newDatasets = props.data.map(d => ({
+            const newDatasets = props.data.map(d => {
+                const color = props.randColor()
+                return ({
                     label: d.name,
-                    backgroundColor: props.randColor(),
+                    backgroundColor: color,
                     barPercentage: 0.5,
                     maxBarThickness: 150,
                     minBarLength: 2,
                     data: [d.vitalityAge, d.realAge]
-                }))
+                })
+            })
             return {
                 data: newDatasets
             }
@@ -26,6 +31,8 @@ export default class MyChart extends Component {
         return (
             <div>
                 <Bar 
+                    redraw
+                    ref={this.theChart}
                     data={{
                     labels: ["Vitality Age", "Real Age"],
                     datasets: this.state.data,
